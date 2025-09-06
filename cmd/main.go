b38d6dc9d83eeb188e8a87191081992237687fa3
z6wdc <195cho@gmail.com>
2025-09-07 00:29:18 +0900

    main entry point

---
+++ cmd/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// This is the main entry point of the application.
+	println("Hello, World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/z6wdc/go-escape-analysis
+
+go 1.24.2
